11d5d721d35d9df372d03dbf21ea6837407b5251
cellstream <cellstream@gmail.com>
2017-11-03 00:44:21 +0100

    Separate non-listen functions into utils.py (#38)
    * Separate non-listen functions into utils.py

--- bertil.py
+++ bertil.py
@@ -5,7 +5,6 @@ import time
 import urllib.request
 import urllib.parse
 import urllib.error
-import json
 import socket
 import re
 import random
@@ -19,95 +18,8 @@ from tinydb import TinyDB, Query
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 
+import utils
 import bertil_secrets
-
-
-def fetch_food_json():
-    response = urllib.request.urlopen('http://www.hanssonohammar.se/veckansmeny.json')
-    return json.loads(response.read().decode('utf-8'))
-
-
-def get_food_from_json(data, day):
-    if day not in data:
-        return "(no mat " + str(day) + ")"
-
-    mat_today = data[day][0]
-
-    if 'IKSU' not in mat_today:
-        return "(no IKSU today)"
-
-    return "\n".join(mat_today['IKSU'])
-
-
-def get_food(day):
-    # Get JSON
-    data = fetch_food_json()
-    return get_food_from_json(data, day)
-
-# Takes a year, returns a year worth of json from api.dryg.net
-# See https://api.dryg.net/ for json format
-def get_swedish_year(year):
-    return requests.get('https://api.dryg.net/dagar/v2.1/{year}'.format(
-        year=year)).json()
-
-# Takes a year and a month, returns a month worth of json from api.dryg.net
-def get_swedish_month(year, month):
-    return requests.get('https://api.dryg.net/dagar/v2.1/{year}/{month}'.format(
-        year=year, month=month)).json()
-
-# Takes a year, a month and a day, returns an array containing a weeks
-# worth of json aquired from api.dryg.net representing the week in which
-# the date sent to function resides
-def get_swedish_week(year, month, day):
-    month_json = get_swedish_month(year, month)['dagar']
-    month_before_json = None
-    month_after_json = None
-
-    date = datetime.date(year, month, day)
-
-    if month == 1:
-        month_before_json = get_swedish_month(year - 1, 12)['dagar']
-        month_after_json = get_swedish_month(year, month + 1)['dagar']
-    elif month == 12:
-        month_before_json = get_swedish_month(year, month - 1)['dagar']
-        month_after_json = get_swedish_month(year + 1, 1)['dagar']
-    else:
-        month_before_json = get_swedish_month(year, month - 1)['dagar']
-        month_after_json = get_swedish_month(year, month + 1)['dagar']
-
-    today_index = len(month_before_json) + date.day - 1
-    monday_index = today_index - date.weekday()
-    sunday_index = today_index + (7 - date.weekday())
-
-    all_days = sum([month_before_json, month_json, month_after_json], [])
-
-    return all_days[monday_index:sunday_index]
-
-# Returns an array containing the current week in json from api.dryg.net
-def get_current_swedish_week():
-    today = datetime.datetime.today()
-    return get_swedish_week(today.year, today.month, today.day)
-
-# Takes a year, month and day, returns the json string representing
-# this day aquired from api.dryg.net
-def get_swedish_day(year, month, day):
-    return requests.get('https://api.dryg.net/dagar/v2.1/{year}/{month}/{day}'.format(
-        year=year, month=month, day=day)).json()
-
-# Takes an array with a weeks worth of json from api.dryg.net along
-# with a weekday index. Makes a shitty guess and returns True if the
-# day is a squeeze day, False otherwise, _highly_ unreliable
-def is_squeeze_day(weekday_index, week_json):
-    day_before = weekday_index - 1 if weekday_index > 0 else weekday_index
-    day_after = weekday_index + 1 if weekday_index < 6 else weekday_index
-
-    return (week_json[day_before]['röd dag'] == 'Ja' or
-            week_json[day_after]['röd dag'] == 'Ja')
-
-# Takes a day json string from api.dryg.net
-# Returns True if day is Workfree, False otherwise
-def is_workfree_day(day):
-    return day['arbetsfri dag'] == 'Ja'
 
 @listen_to(r'^help$')
 def bertil_help(message):
@@ -125,14 +37,14 @@ def veckans_mat(message):
         nextweek = 7 - today
         today = 0
 
-    data = fetch_food_json()
+    data = utils.fetch_food_json()
 
     fulltext = ""
     for daynum in range(0, len(days) - today):
         date = datetime.date.fromtimestamp(time.time() + (86400 * nextweek) + (86400 * daynum))
         try:
             fulltext += "\n{}\n{}\n".format(days[today+daynum],
-                                            get_food_from_json(data, str(date)))
+                                            utils.get_food_from_json(data, str(date)))
         except Exception as exception:
             if str(exception):
                 fulltext += "\n{}\n{}\n".format(days[today+daynum], str(exception))
@@ -161,7 +73,7 @@ def datum(message):
 def mat(message, plus):
     date = datetime.date.fromtimestamp(time.time() + (86400 * len(plus)))
     try:
-        message.reply("```IKSU - {}\n{}```".format(str(date), get_food(str(date))))
+        message.reply("```IKSU - {}\n{}```".format(str(date), utils.get_food(str(date))))
     except Exception as exception:
         message.reply("Kom inte åt maten 😞 ({what})".format(what=str(exception)))
 
@@ -224,13 +136,13 @@ def hem(message):
 @listen_to(r'^n[\u00E4\u00C4]r.*helg.*\?', re.IGNORECASE)
 def whenhelg(message):
     today = datetime.datetime.now()
-    week = get_current_swedish_week()
+    week = utils.get_current_swedish_week()
     days = 0
 
     for day in week[today.weekday():len(week)]:
         day_index = int(day['dag i vecka']) - 1
-        if (not is_workfree_day(day) and not
-                is_squeeze_day(day_index, week)):
+        if (not utils.is_workfree_day(day) and not
+                utils.is_squeeze_day(day_index, week)):
             days += 1
 
     hours = 0
@@ -246,10 +158,13 @@ def whenhelg(message):
                  ':disappointed::noose:',
                 ]
 
-    message.reply('Det är {days} dagar och {hours} timmar kvar till helgen...' \
-                  '{reaction}'.format(days=days,
-                                      hours=hours,
-                                      reaction=reactions[days]))
+    if days == 0 and hours == 0:
+        message.reply('Det är ju redan helg! :kreygasm:')
+    else:
+        message.reply('Det är {days} dagar och {hours} timmar kvar till' \
+                      'helgen... {reaction}'.format(days=days,
+                                                    hours=hours,
+                                                    reaction=reactions[days]))
 
 @listen_to(r'^temp$')
 def temp(message):
---
+++ utils.py
@@ -0,0 +1,97 @@
+import urllib.request
+import json
+import requests
+
+def fetch_food_json():
+    response = urllib.request.urlopen('http://www.hanssonohammar.se/veckansmeny.json')
+    return json.loads(response.read().decode('utf-8'))
+
+
+def get_food_from_json(data, day):
+    if day not in data:
+        return "(no mat " + str(day) + ")"
+
+    mat_today = data[day][0]
+
+    if 'IKSU' not in mat_today:
+        return "(no IKSU today)"
+
+    return "\n".join(mat_today['IKSU'])
+
+
+def get_food(day):
+    # Get JSON
+    data = fetch_food_json()
+    return get_food_from_json(data, day)
+
+
+# Takes a year, returns a year worth of json from api.dryg.net
+# See https://api.dryg.net/ for json format
+def get_swedish_year(year):
+    return requests.get('https://api.dryg.net/dagar/v2.1/{year}'.format(
+        year=year)).json()
+
+
+# Takes a year and a month, returns a month worth of json from api.dryg.net
+def get_swedish_month(year, month):
+    return requests.get('https://api.dryg.net/dagar/v2.1/{year}/{month}'.format(
+        year=year, month=month)).json()
+
+
+# Takes a year, a month and a day, returns an array containing a weeks
+# worth of json aquired from api.dryg.net representing the week in which
+# the date sent to function resides
+def get_swedish_week(year, month, day):
+    month_json = get_swedish_month(year, month)['dagar']
+    month_before_json = None
+    month_after_json = None
+
+    date = datetime.date(year, month, day)
+
+    if month == 1:
+        month_before_json = get_swedish_month(year - 1, 12)['dagar']
+        month_after_json = get_swedish_month(year, month + 1)['dagar']
+    elif month == 12:
+        month_before_json = get_swedish_month(year, month - 1)['dagar']
+        month_after_json = get_swedish_month(year + 1, 1)['dagar']
+    else:
+        month_before_json = get_swedish_month(year, month - 1)['dagar']
+        month_after_json = get_swedish_month(year, month + 1)['dagar']
+
+    today_index = len(month_before_json) + date.day - 1
+    monday_index = today_index - date.weekday()
+    sunday_index = today_index + (7 - date.weekday())
+
+    all_days = sum([month_before_json, month_json, month_after_json], [])
+
+    return all_days[monday_index:sunday_index]
+
+
+# Returns an array containing the current week in json from api.dryg.net
+def get_current_swedish_week():
+    today = datetime.datetime.today()
+    return get_swedish_week(today.year, today.month, today.day)
+
+
+# Takes a year, month and day, returns the json string representing
+# this day aquired from api.dryg.net
+def get_swedish_day(year, month, day):
+    return requests.get('https://api.dryg.net/dagar/v2.1/{year}/{month}/{day}'.format(
+        year=year, month=month, day=day)).json()
+
+
+# Takes an array with a weeks worth of json from api.dryg.net along
+# with a weekday index. Makes a shitty guess and returns True if the
+# day is a squeeze day, False otherwise, _highly_ unreliable
+def is_squeeze_day(weekday_index, week_json):
+    day_before = weekday_index - 1 if weekday_index > 0 else weekday_index
+    day_after = weekday_index + 1 if weekday_index < 6 else weekday_index
+
+    return (week_json[day_before]['röd dag'] == 'Ja' or
+            week_json[day_after]['röd dag'] == 'Ja')
+
+
+# Takes a day json string from api.dryg.net
+# Returns True if day is Workfree, False otherwise
+def is_workfree_day(day):
+    return day['arbetsfri dag'] == 'Ja'
